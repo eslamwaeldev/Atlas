dde2a04b65303671667e9373bd68f4f075a780d1
Eslam Wael <eslam.wael104@gmail.com>
2023-02-03 23:11:09 +0200

    feat: add integration with open weather api and unsplash api

--- src/pages/[country].tsx
+++ src/pages/[country].tsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
 import useSWR from "swr";
+import Image from "next/image";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const fetchCountry = await fetch(`https://restcountries.com/v3.1/name/${params?.country}`);
@@ -15,13 +16,25 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       notFound: true,
     };
   }
+  const fetchWeather = await fetch(
+    `https://api.openweathermap.org/data/2.5/find?q=${countryData?.[0]?.name?.official}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`
+  );
+  const weatherData = await fetchWeather.json();
+  const fetchImages = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${
+      `landmarks in ` + countryData?.[0].name?.common
+    }&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_KEY}`
+  );
+  const countryImages = await fetchImages.json();
   return {
-    props: { countryData },
+    props: { countryData, weatherData, countryImages },
   };
 };
 
 export default function CountryPage({
   countryData,
+  weatherData,
+  countryImages,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { country } = router.query;
@@ -31,6 +44,8 @@ export default function CountryPage({
   );
   if (error) router.push(`/404`);
   if (isLoading) return <div>Loading...</div>;
+  console.log(`Images: `, countryImages);
+  console.log(`weather Data: `, weatherData);
   return (
     <>
       <Head>
@@ -40,8 +55,13 @@ export default function CountryPage({
       </Head>
       <main>
         <section>
-          <div className="w-screen h-screen flex justify-center items-center">
+          <div className="w-screen h-screen flex flex-col gap-5 justify-center items-center">
             <h1 className="text-red-900 text-7xl">{countryData[0]?.name?.official}</h1>
+            <img
+              className="w-1/2 h-1/2"
+              src={countryImages?.results?.[0].urls?.full}
+              alt="country Image"
+            />
           </div>
         </section>
       </main>
