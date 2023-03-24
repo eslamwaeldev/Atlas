618e9ec15f9b0e02b020b4491a9fbcd333d3391e
Eslam Wael <eslam.wael104@gmail.com>
2023-03-24 22:44:43 +0200

    feat: add _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { inter } from "lib/fonts/fonts";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  );
 }
