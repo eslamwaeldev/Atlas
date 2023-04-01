3b72ed092c71c815cda347f4b91efa5173ff75aa
Eslam Wael <eslam.wael104@gmail.com>
2023-04-01 22:46:45 +0200

    refactor: remove dummy object and fix: fix font family bug

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,7 +1,6 @@
 import { Fragment, useEffect, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { useRouter } from "next/router";
 
 type name = {
@@ -11,15 +10,6 @@ type name = {
 type Country = {
   name: name;
 };
-
-const countries = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-];
 
 export default function SearchBox() {
   const [countries, setCountries] = useState<Country[]>([]);
@@ -55,7 +45,7 @@ export default function SearchBox() {
           router.push(`/${event.name.common}`);
         }}
       >
-        <div className="relative mt-1">
+        <div className="relative mt-1 font-sans">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-atlas-gold sm:text-sm">
             <Combobox.Input
               className="w-full h-10 border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
