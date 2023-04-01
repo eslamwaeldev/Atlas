afba5ba581f8d5b00e16e3a3ef5aedd7dff6cf74
Eslam Wael <eslam.wael104@gmail.com>
2023-04-01 22:47:18 +0200

    fix: fix getserver side props not getting types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
-import { inter } from "lib/fonts/fonts";
+import { inter, lora } from "lib/fonts/fonts";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+
+export type NextPageWithLayout<Props> = NextPage<Props> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main>
+    <main className={`${inter.variable} ${lora.variable}`}>
       <Component {...pageProps} />
     </main>
   );
