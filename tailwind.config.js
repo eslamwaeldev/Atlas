885483261e33631956fd0dc26bd89fcd03cca0e6
Eslam Wael <eslam.wael104@gmail.com>
2023-04-01 22:40:46 +0200

    feat: update website theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,25 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      top: {
+        arrow: "3.3rem",
+      },
+      height: {
+        100: "26rem",
+      },
+      width: {
+        100: "28rem",
+        101: "31rem",
+        102: "32rem",
+        104: "35rem",
+        110: "72rem",
+      },
+
       backgroundImage: {
         "atlas-world": "url('/images/atlasBG.png')",
         "atlas-intro": "linear-gradient(116.82deg, #1E1719 66.98%, #172027 87.29%, #112732 100%)",
         "atlas-search": "linear-gradient(243.18deg, #112732 19.06%, #1E171C 84.17%)",
+        "atlas-404": "linear-gradient(243.18deg, #112732 19.06%, #172028 34.17%, #1E171C 63.33%);",
       },
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
